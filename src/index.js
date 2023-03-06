7bcac6b2ea677b8ffae11e4fc5a629c0b40652b5
Gary <geehor@gmail.com>
2023-03-06 16:37:21 -0500

    Start drawing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import MovingObject from "./moving_object.js";
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function() {
+  const canvasEl = document.getElementById("game-canvas");
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+  const ctx = canvasEl.getContext("2d")
+
+})
--- src/moving_object.js
+++ src/moving_object.js
@@ -4,6 +4,21 @@ class MovingObject{
         this.properties = properties;    
     }
 
+    draw(ctx) {
+        ctx.fillStyle = this.properties[color];
+        ctx.beginPath();
+
+        ctx.arc(
+            this.properties[pos][0],
+            this.properties[pos][1],
+            this.properties[radius],
+            2 * Math.PI
+            // false
+        );
+
+        ctx.fill();
+    }
+
 }
 
 export default MovingObject;
